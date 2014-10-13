beed5b1d6e3bc33d95429fc3342fb317a8d0cf66
Timo Vesalainen <timo.vesalainen@iki.fi>
2014-10-13 12:46:07 +0100

    Add MatrixSort class for sorting DenseMatrix64F rows.

---
+++ src/main/java/org/vesalainen/util/MatrixSort.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright (C) 2014 Timo Vesalainen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.vesalainen.util;
+
+import org.ejml.data.DenseMatrix64F;
+
+/**
+ * MatrixSort is able to sort matrix rows when matrix is stored is one dimensional 
+ * array as in DenseMatrix64F.
+ * 
+ * <p>Sort is using Quick Sort algorithm.
+ * @author Timo Vesalainen
+ * @see org.ejml.data.DenseMatrix64F
+ */
+public class MatrixSort
+{
+    /**
+     * Sorts matrix rows in ascending order using given comparator.
+     * @param matrix
+     * @param comparator 
+     */
+    public static void sort(DenseMatrix64F matrix, RowComparator comparator)
+    {
+        int len = matrix.numCols;
+        quickSort(matrix.data, 0, matrix.getNumElements()-1, len, comparator, new double[len], new double[len]);
+    }
+    /**
+     * Sorts rows in 1D array in ascending order using given comparator.
+     * @param data
+     * @param rowLength
+     * @param comparator 
+     */
+    public static void sort(double[] data, int rowLength, RowComparator comparator)
+    {
+        quickSort(data, 0, data.length - 1, rowLength, comparator, new double[rowLength], new double[rowLength]);
+    }
+
+    private static void quickSort(double[] arr, int left, int right, int len, RowComparator c, double[] pivot, double[] tmp)
+    {
+        int i = left, j = right;
+        System.arraycopy(arr, (left + right) / 2, pivot, 0, len);
+
+        /* partition */
+        while (i <= j)
+        {
+            while (c.compare(arr, i, pivot, len) < 0)
+            {
+                i++;
+            }
+            while (c.compare(arr, j, pivot, len) > 0)
+            {
+                j--;
+            }
+            if (i <= j)
+            {
+                if (i != j)
+                {
+                    System.arraycopy(arr, i, tmp, 0, len);
+                    System.arraycopy(arr, j, arr, i, len);
+                    System.arraycopy(tmp, 0, arr, j, len);
+                }
+                i++;
+                j--;
+            }
+        };
+
+        /* recursion */
+        if (left < j)
+        {
+            quickSort(arr, left, j, len, c, pivot, tmp);
+        }
+        if (i < right)
+        {
+            quickSort(arr, i, right, len, c, pivot, tmp);
+        }
+    }
+
+    public interface RowComparator
+    {
+        /**
+         * Compares sub array of data to pivot array.
+         * 
+         * <p>Returns -1 if data[row*len,...,(row+1)*len-1] &lt; pivot.
+         * <p>Returns 1 if data[row*len,...,(row+1)*len-1] &gt; pivot.
+         * <p>Returns 0 if data[row*len,...,(row+1)*len-1] == pivot.
+         * @param data
+         * @param row
+         * @param pivot
+         * @param len
+         * @return 
+         */
+        int compare(double[] data, int row, double[] pivot, int len);
+    }
+}
---
+++ src/test/java/org/vesalainen/util/MatrixSortTest.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (C) 2014 Timo Vesalainen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.vesalainen.util;
+
+import java.util.Arrays;
+import java.util.Random;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.vesalainen.util.MatrixSort.RowComparator;
+
+/**
+ *
+ * @author Timo Vesalainen
+ */
+public class MatrixSortTest
+{
+
+    public MatrixSortTest()
+    {
+    }
+
+    /**
+     * Test of sort method, of class MatrixSort.
+     */
+    @Test
+    public void testSort1()
+    {
+        double[] arr1 = new double[]
+        {
+            1, 12, 5, 26, 7, 14, 3, 7, 2, 23, 56
+        };
+        double[] arr2 = new double[arr1.length];
+        System.arraycopy(arr1, 0, arr2, 0, arr1.length);
+        MatrixSort.sort(arr1, 1, new C());
+        Arrays.sort(arr2);
+        assertTrue(Arrays.equals(arr2, arr1));
+    }
+
+    @Test
+    public void testSort2()
+    {
+        double[] arr1 = new double[1000];
+        double[] arr2 = new double[arr1.length];
+        Random r = new Random(1234567L);
+        for (int ii = 0; ii < arr1.length; ii++)
+        {
+            arr1[ii] = r.nextDouble();
+        }
+        System.arraycopy(arr1, 0, arr2, 0, arr1.length);
+        MatrixSort.sort(arr1, 1, new C());
+        Arrays.sort(arr2);
+        assertTrue(Arrays.equals(arr2, arr1));
+    }
+
+    public class C implements RowComparator
+    {
+
+        @Override
+        public int compare(double[] data, int row, double pivot[], int len)
+        {
+            if (data[row] < pivot[0])
+            {
+                return -1;
+            }
+            else
+            {
+                if (data[row] == pivot[0])
+                {
+                    return 0;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+        }
+
+    }
+
+}
