d5080cdee47a29e947644557ee3f782de930b93d
Timo Vesalainen <timo.vesalainen@iki.fi>
2015-04-15 12:49:14 +0100

    Add Pair

---
+++ src/main/java/org/vesalainen/util/Pair.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2015 tkv
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.vesalainen.util;
+
+/**
+ *
+ * @author tkv
+ */
+public class Pair<F,S>
+{
+    private F first;
+    private S second;
+
+    public Pair(F first, S second)
+    {
+        this.first = first;
+        this.second = second;
+    }
+
+    public F getFirst()
+    {
+        return first;
+    }
+
+    public void setFirst(F first)
+    {
+        this.first = first;
+    }
+
+    public S getSecond()
+    {
+        return second;
+    }
+
+    public void setSecond(S second)
+    {
+        this.second = second;
+    }
+    
+}
