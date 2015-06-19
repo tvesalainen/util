9196c1d692d01b53a76b4540f02e0e2f4a51fc43
Timo Vesalainen <timo.vesalainen@iki.fi>
2015-06-19 13:24:37 +0100

    Add ConcurrentArraySet

---
+++ src/main/java/org/vesalainen/util/concurrent/ConcurrentArraySet.java
@@ -0,0 +1,278 @@
+/*
+ * Copyright (C) 2015 tkv
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.vesalainen.util.concurrent;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock.ReadLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock.WriteLock;
+
+/**
+ * A class optimized for fast iterator. It's methods are thread-safe.
+ * <p>Iterators are not fail-fast. Instead they are using read/write locks.
+ * Iterator is write-locked causing other threads to wait. Lock is held until
+ * hasNext returns true. Application should call unlock if it exists iterator loop.
+ * @author tkv
+ */
+public class ConcurrentArraySet<T> implements Set<T>
+{
+    private final List<T> list = new ArrayList<>();
+    private Iter iterator;
+    private final ReentrantReadWriteLock rwLock = new ReentrantReadWriteLock();
+    private final ReadLock readLock = rwLock.readLock();
+    private final WriteLock writeLock = rwLock.writeLock();
+    
+    @Override
+    public int size()
+    {
+        return list.size();
+    }
+
+    @Override
+    public boolean isEmpty()
+    {
+        return list.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o)
+    {
+        readLock.lock();
+        try
+        {
+            return list.contains(o);
+        }
+        finally
+        {
+            readLock.unlock();
+        }
+    }
+    /**
+     * Returned iterator is write-locked until hasNext returns false or unlock
+     * method is called.
+     * @return 
+     */
+    @Override
+    public Iterator<T> iterator()
+    {
+        writeLock.lock();
+        if (iterator == null)
+        {
+            iterator = new Iter();
+        }
+        else
+        {
+            iterator.reset();
+        }
+        return iterator;
+    }
+    /**
+     * Unlock write-lock locked by iterator. It is safe to call this method 
+     * several times. If the current thread is not holding the lock it does nothing.
+     */
+    public void unlock()
+    {
+        if (writeLock.isHeldByCurrentThread())
+        {
+            writeLock.unlock();
+        }
+    }
+    
+    @Override
+    public Object[] toArray()
+    {
+        readLock.lock();
+        try
+        {
+            return list.toArray();
+        }
+        finally
+        {
+            readLock.unlock();
+        }
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a)
+    {
+        readLock.lock();
+        try
+        {
+            return list.toArray(a);
+        }
+        finally
+        {
+            readLock.unlock();
+        }
+    }
+
+    @Override
+    public boolean add(T e)
+    {
+        writeLock.lock();
+        try
+        {
+            if (!list.contains(e))
+            {
+                list.add(e);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        finally
+        {
+            writeLock.unlock();
+        }
+    }
+
+    @Override
+    public boolean remove(Object o)
+    {
+        writeLock.lock();
+        try
+        {
+            return list.remove(o);
+        }
+        finally
+        {
+            writeLock.unlock();
+        }
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c)
+    {
+        readLock.lock();
+        try
+        {
+            return list.containsAll(c);
+        }
+        finally
+        {
+            readLock.unlock();
+        }
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c)
+    {
+        writeLock.lock();
+        try
+        {
+            boolean changed = false;
+            for (T o : c)
+            {
+                if (add(o))
+                {
+                    changed = true;
+                }
+            }
+            return changed;
+        }
+        finally
+        {
+            writeLock.unlock();
+        }
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c)
+    {
+        writeLock.lock();
+        try
+        {
+            return list.retainAll(c);
+        }
+        finally
+        {
+            writeLock.unlock();
+        }
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c)
+    {
+        writeLock.lock();
+        try
+        {
+            return list.removeAll(c);
+        }
+        finally
+        {
+            writeLock.unlock();
+        }
+    }
+
+    @Override
+    public void clear()
+    {
+        writeLock.lock();
+        try
+        {
+            list.clear();
+        }
+        finally
+        {
+            writeLock.unlock();
+        }
+    }
+
+    private class Iter implements Iterator<T>
+    {
+        private int index = size();
+        public Iter()
+        {
+        }
+
+        private void reset()
+        {
+            index = size();
+        }
+
+        @Override
+        public boolean hasNext()
+        {
+            boolean hasNext = index > 0;
+            if (!hasNext)
+            {
+                writeLock.unlock();
+            }
+            return hasNext;
+        }
+
+        @Override
+        public T next()
+        {
+            return list.get(--index);
+        }
+
+        @Override
+        public void remove()
+        {
+            list.remove(index);
+        }
+        
+    }
+    
+}
---
+++ src/test/java/org/vesalainen/util/concurrent/ConcurrentArraySetTest.java
@@ -0,0 +1,221 @@
+/*
+ * Copyright (C) 2015 tkv
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.vesalainen.util.concurrent;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author tkv
+ */
+public class ConcurrentArraySetTest
+{
+    
+    public ConcurrentArraySetTest()
+    {
+    }
+
+    @Test
+    public void test1()
+    {
+        ConcurrentArraySet<String> cas = new ConcurrentArraySet<>();
+        assertTrue(cas.add("foo"));
+        assertTrue(cas.contains("foo"));
+        assertFalse(cas.contains("bar"));
+        assertFalse(cas.isEmpty());
+        assertEquals(1, cas.size());
+        assertFalse(cas.add("foo"));
+        assertEquals(1, cas.size());
+        assertTrue(cas.add("bar"));
+        assertEquals(2, cas.size());
+        assertTrue(cas.remove("foo"));
+        assertEquals(1, cas.size());
+        assertFalse(cas.contains("foo"));
+        cas.clear();
+        assertFalse(cas.contains("foo"));
+        assertFalse(cas.contains("bar"));
+        assertTrue(cas.isEmpty());
+    }
+    
+    @Test
+    public void test2()
+    {
+        List<Integer> f = new ArrayList<>();
+        f.add(1);
+        f.add(1);
+        f.add(2);
+        f.add(3);
+        f.add(5);
+        f.add(8);
+        f.add(13);
+        f.add(21);
+        List<Integer> o = new ArrayList<>();
+        o.add(1);
+        o.add(3);
+        o.add(5);
+        o.add(7);
+        o.add(9);
+        o.add(11);
+        o.add(13);
+        ConcurrentArraySet<Integer> cas = new ConcurrentArraySet<>();
+        assertTrue(cas.addAll(f));
+        assertEquals(7, cas.size());
+        assertTrue(cas.addAll(o));
+        assertFalse(cas.addAll(o));
+        assertEquals(10, cas.size());
+        assertTrue(cas.containsAll(f));
+        assertTrue(cas.containsAll(o));
+        cas.retainAll(o);
+        assertTrue(o.containsAll(cas));
+        assertTrue(cas.containsAll(o));
+        assertTrue(cas.removeAll(f));
+        assertEquals(3, cas.size());
+        assertTrue(cas.contains(7));
+        assertTrue(cas.contains(9));
+        assertTrue(cas.contains(11));
+    }
+    @Test
+    public void test3()
+    {
+        List<Integer> o = new ArrayList<>();
+        o.add(1);
+        o.add(3);
+        o.add(11);
+        o.add(3);
+        o.add(5);
+        o.add(7);
+        o.add(11);
+        o.add(7);
+        o.add(9);
+        o.add(11);
+        o.add(13);
+        Set<Integer> exp = new HashSet<>();
+        exp.addAll(o);
+        Set<Integer> got = new HashSet<>();
+        ConcurrentArraySet<Integer> cas = new ConcurrentArraySet<>();
+        cas.addAll(o);
+        for (Integer i : cas)
+        {
+            got.add(i);
+        }
+        assertEquals(exp, got);
+        got.clear();
+        for (Integer i : cas)
+        {
+            got.add(i);
+        }
+        assertEquals(exp, got);
+    }
+    @Test
+    public void test4()
+    {
+        List<Integer> f = new ArrayList<>();
+        f.add(1);
+        f.add(1);
+        f.add(2);
+        f.add(3);
+        f.add(5);
+        f.add(8);
+        f.add(13);
+        f.add(21);
+        List<Integer> o = new ArrayList<>();
+        o.add(1);
+        o.add(3);
+        o.add(5);
+        o.add(7);
+        o.add(9);
+        o.add(11);
+        o.add(13);
+        ConcurrentArraySet<Integer> cas = new ConcurrentArraySet<>();
+        cas.addAll(f);
+        cas.addAll(o);
+        Set<Integer> exp = new HashSet<>();
+        exp.addAll(f);
+        exp.addAll(o);
+        exp.removeAll(o);
+        Iterator<Integer> iterator = cas.iterator();
+        while (iterator.hasNext())
+        {
+            Integer i = iterator.next();
+            if (o.contains(i))
+            {
+                iterator.remove();
+            }
+        }
+        assertEquals(exp.size(), cas.size());
+        assertTrue(cas.containsAll(exp));
+        assertTrue(exp.containsAll(cas));
+    }
+    @Test
+    public void test5()
+    {
+        ConcurrentArraySet<Integer> cas = new ConcurrentArraySet<>();
+        cas.add(1);
+        cas.add(3);
+        cas.add(5);
+        cas.add(7);
+        cas.add(9);
+        cas.add(11);
+        cas.add(13);
+        try
+        {
+            for (Integer i : cas)
+            {
+                if (i == 7)
+                {
+                    break;
+                }
+            }
+        }
+        finally
+        {
+            cas.unlock();
+        }
+    }
+    @Test
+    public void test6()
+    {
+        ConcurrentArraySet<Integer> cas = new ConcurrentArraySet<>();
+        cas.add(1);
+        cas.add(3);
+        cas.add(5);
+        cas.add(7);
+        cas.add(9);
+        cas.add(11);
+        cas.add(13);
+        try
+        {
+            for (Integer i : cas)
+            {
+                if (i == 0)
+                {
+                    break;
+                }
+            }
+        }
+        finally
+        {
+            cas.unlock();
+        }
+    }
+}
