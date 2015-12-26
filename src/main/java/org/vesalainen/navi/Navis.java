29b7f209f2822b892adff60c6873083c522d2688
Timo Vesalainen <timo.vesalainen@iki.fi>
2015-12-26 05:16:50 -0300

    Moved common navigational methods to Navis

--- src/main/java/org/vesalainen/navi/Navis.java
+++ src/main/java/org/vesalainen/navi/Navis.java
@@ -16,15 +16,58 @@
  */
 package org.vesalainen.navi;
 
+import java.util.concurrent.TimeUnit;
+
 /**
- *
+ * Collection of navigational methods etc.
+ * 
+ * <p>coordinates are in degrees. Positive values are north and east.
+ * 
  * @author tkv
  */
 public class Navis
 {
+    public static final double Kilo = 1000;
+    public static final double NMInMeters = 1852;
+    public static final double FeetInMeters = 0.3048;
+    public static final double FathomInMeters = 1.8288;
+    public static final double HoursInSecond = TimeUnit.HOURS.toSeconds(1);
+    public static final double NMInMetersPerHoursInSecond = NMInMeters / HoursInSecond;
+    /**
+     * Return average departure of two waypoints
+     * @param loc1
+     * @param loc2
+     * @return 
+     */
     public static final double departure(WayPoint loc1, WayPoint loc2)
     {
-        return Math.cos(Math.toRadians((loc2.getLatitude()+loc1.getLatitude())/2));
+        return departure(loc2.getLatitude(), loc1.getLatitude());
+    }
+    /**
+     * Returns departure of average latitude
+     * @param latitude1
+     * @param latitude2
+     * @return 
+     */
+    public static final double departure(double latitude1, double latitude2)
+    {
+        checkLatitude(latitude1);
+        checkLatitude(latitude2);
+        return departure((latitude1+latitude2)/2);
+    }
+    /**
+     * Returns departure of latitude
+     * <p>Departure is used in calculating distances in longitude. While 
+     * (lat1-lat2)/60 is distance in NM, departure*(lon1-lon2)/60 is also 
+     * distance in NM
+     * 
+     * @param latitude
+     * @return 
+     */
+    public static final double departure(double latitude)
+    {
+        checkLatitude(latitude);
+        return Math.cos(Math.toRadians(latitude));
     }
     /**
      * Return bearing from wp1 to wp2 in degrees
@@ -34,9 +77,29 @@ public class Navis
      */
     public static final double bearing(WayPoint wp1, WayPoint wp2)
     {
-        double dep = departure(wp1, wp2);
-        double aa = dep*(wp2.getLongitude()-wp1.getLongitude());
-        double bb = wp2.getLatitude()-wp1.getLatitude();
+        double lat1 = wp1.getLatitude();
+        double lat2 = wp2.getLatitude();
+        double lon1 = wp1.getLongitude();
+        double lon2 = wp2.getLongitude();
+        return bearing(lat1, lon1, lat2, lon2);
+    }
+    /**
+     * Returns bearing from (lat1, lon1) to (lat2, lon2) in degrees
+     * @param lat1
+     * @param lon1
+     * @param lat2
+     * @param lon2
+     * @return 
+     */
+    public static final double bearing(double lat1, double lon1, double lat2, double lon2)
+    {
+        checkLatitude(lat1);
+        checkLongitude(lon1);
+        checkLatitude(lat2);
+        checkLongitude(lon2);
+        double dep = departure(lat1, lat2);
+        double aa = dep*(lon2-lon1);
+        double bb = lat2-lat1;
         double dd = Math.atan2(aa, bb);
         if (dd < 0)
         {
@@ -52,10 +115,30 @@ public class Navis
      */
     public static final double distance(WayPoint wp1, WayPoint wp2)
     {
-        double dep = departure(wp1, wp2);
+        double lat1 = wp1.getLatitude();
+        double lat2 = wp2.getLatitude();
+        double lon1 = wp1.getLongitude();
+        double lon2 = wp2.getLongitude();
+        return distance(lat1, lon1, lat2, lon2);
+    }
+    /**
+     * Returns distance between (lat1, lon1) and (lat2, lon2) in NM
+     * @param lat1
+     * @param lon1
+     * @param lat2
+     * @param lon2
+     * @return 
+     */
+    public static final double distance(double lat1, double lon1, double lat2, double lon2)
+    {
+        checkLatitude(lat1);
+        checkLongitude(lon1);
+        checkLatitude(lat2);
+        checkLongitude(lon2);
+        double dep = departure(lat1, lat2);
         return 60*Math.hypot(
-                wp1.getLatitude()-wp2.getLatitude(),
-                dep*(wp1.getLongitude()-wp2.getLongitude())
+                lat1-lat2,
+                dep*(lon1-lon2)
                 );
     }
     /**
@@ -66,11 +149,123 @@ public class Navis
      */
     public static final double speed(WayPoint wp1, WayPoint wp2)
     {
-        double distance = distance(wp1, wp2);
-        double duration = wp2.getTime()-wp1.getTime();
+        double lat1 = wp1.getLatitude();
+        double lat2 = wp2.getLatitude();
+        double lon1 = wp1.getLongitude();
+        double lon2 = wp2.getLongitude();
+        long time1 = wp1.getTime();
+        long time2 = wp2.getTime();
+        return speed(time1, lat1, lon1, time2, lat2, lon2);
+    }
+    /**
+     * Returns the speed needed to move from (time1, lat1, lon1) to (time2, lat2, lon2)
+     * @param time1
+     * @param lat1
+     * @param lon1
+     * @param time2
+     * @param lat2
+     * @param lon2
+     * @return Knots
+     */
+    public static final double speed(long time1, double lat1, double lon1, long time2, double lat2, double lon2)
+    {
+        checkLatitude(lat1);
+        checkLongitude(lon1);
+        checkLatitude(lat2);
+        checkLongitude(lon2);
+        double distance = distance(lat1, lon1, lat2, lon2);
+        double duration = time2-time1;
         double hours = duration/3600000.0;
         double speed = distance/hours;
         return speed;
     }
-    
+    /**
+     * Converts fathoms to meters
+     * @param fathom
+     * @return 
+     */
+    public static final double fathomsToMeters(double fathom)
+    {
+        return fathom*FathomInMeters;
+    }
+    /**
+     * Converts meters to fathoms
+     * @param meters
+     * @return 
+     */
+    public static final double metersToFathoms(double meters)
+    {
+        return meters/FathomInMeters;
+    }
+    /**
+     * Converts feet to  meters
+     * @param feets
+     * @return 
+     */
+    public static final double feetsToMeters(double feets)
+    {
+        return feets*FeetInMeters;
+    }
+    /**
+     * Converts meters to feet
+     * @param meters
+     * @return 
+     */
+    public static final double metersToFeets(double meters)
+    {
+        return meters/FeetInMeters;
+    }
+    /**
+     * Converts knots to m/s
+     * @param knots
+     * @return 
+     */
+    public static final double knotsToMetersPerSecond(double knots)
+    {
+        return NMInMetersPerHoursInSecond*knots;
+    }
+    /**
+     * Converts m/s to knots
+     * @param ms
+     * @return 
+     */
+    public static final double metersPerSecondToKnots(double ms)
+    {
+        return NMInMetersPerHoursInSecond/ms;
+    }
+
+    private static final double HoursInSecondPerKilo = HoursInSecond / Kilo;
+    /**
+     * Converts m/s to Km/h
+     * @param metersPerSecond
+     * @return 
+     */
+    public static final double metersPerSecondToKiloMetersInHour(double metersPerSecond)
+    {
+        return HoursInSecondPerKilo*metersPerSecond;
+    }
+    /**
+     * Converts Km/h to m/s
+     * @param kmh
+     * @return 
+     */
+    public static final double kiloMetersInHourToMetersPerSecond(double kmh)
+    {
+        return kmh/HoursInSecondPerKilo;
+    }
+
+    private static void checkLatitude(double latitude)
+    {
+        if (latitude > 90 || latitude < -90)
+        {
+            throw new IllegalArgumentException("latitude out of range "+latitude);
+        }
+    }
+    private static void checkLongitude(double longitude)
+    {
+        if (longitude > 180 || longitude < -180)
+        {
+            throw new IllegalArgumentException("longitude out of range "+longitude);
+        }
+    }
 }
---
+++ src/test/java/org/vesalainen/navi/NavisTest.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright (C) 2015 tkv
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.vesalainen.navi;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.vesalainen.util.navi.Fathom;
+import org.vesalainen.util.navi.Feet;
+import org.vesalainen.util.navi.KilometersInHour;
+import org.vesalainen.util.navi.Knots;
+import org.vesalainen.util.navi.Scalar;
+import org.vesalainen.util.navi.Velocity;
+
+/**
+ *
+ * @author tkv
+ */
+public class NavisTest
+{
+    private static final double Epsilon = 1e-10;
+    public NavisTest()
+    {
+    }
+
+    @Test
+    public void testBearing1()
+    {
+        WP wp1 = new WP(0, 60, 25);
+        WP wp2 = new WP(0, 60, 24);
+        assertEquals(270, Navis.bearing(wp1, wp2), Epsilon);
+        assertEquals(90, Navis.bearing(wp2, wp1), Epsilon);
+    }
+    
+    @Test
+    public void testBearing2()
+    {
+        WP wp1 = new WP(0, -0.5F, 24);
+        WP wp2 = new WP(0, 0.5F, 25);
+        assertEquals(45, Navis.bearing(wp1, wp2), Epsilon);
+        assertEquals(225, Navis.bearing(wp2, wp1), Epsilon);
+    }
+    
+    @Test
+    public void testBearing3()
+    {
+        WP wp1 = new WP(0, 59.5F, 24);
+        WP wp2 = new WP(0, 60.5F, 26);
+        assertEquals(45, Navis.bearing(wp1, wp2), Epsilon);
+        assertEquals(225, Navis.bearing(wp2, wp1), Epsilon);
+    }
+    
+    @Test
+    public void testFathom()
+    {
+        assertEquals(1.8288, Navis.fathomsToMeters(1), Epsilon);
+        assertEquals(1, Navis.metersToFathoms(1.8288), Epsilon);
+    }
+    
+    @Test
+    public void testFeets()
+    {
+        assertEquals(0.3048, Navis.feetsToMeters(1), Epsilon);
+        assertEquals(1, Navis.metersToFeets(0.3048), Epsilon);
+    }
+    
+    @Test
+    public void testKnots()
+    {
+        double v = 1852.0/3600.0;
+        assertEquals(v, Navis.knotsToMetersPerSecond(1.0), Epsilon);
+        assertEquals(1, Navis.metersPerSecondToKnots(v), Epsilon);
+    }
+
+    @Test
+    public void testKMH()
+    {
+        double e = 1000.0/3600.0;
+        assertEquals(e, Navis.kiloMetersInHourToMetersPerSecond(1.0), Epsilon);
+        assertEquals(1, Navis.metersPerSecondToKiloMetersInHour(e), Epsilon);
+    }
+
+    private class WP implements WayPoint
+    {
+        private long time;
+        private float latitude;
+        private float longitude;
+
+        public WP(long time, float latitude, float longitude)
+        {
+            this.time = time;
+            this.latitude = latitude;
+            this.longitude = longitude;
+        }
+
+        @Override
+        public long getTime()
+        {
+            return time;
+        }
+
+        @Override
+        public double getLatitude()
+        {
+            return latitude;
+        }
+
+        @Override
+        public double getLongitude()
+        {
+            return longitude;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "WayPoint{" + "time=" + time + ", latitude=" + latitude + ", longitude=" + longitude + '}';
+        }
+        
+    }
+}
