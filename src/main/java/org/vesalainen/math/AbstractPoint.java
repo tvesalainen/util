580e1a95ab793e54005c0658d0cf1a41f27b75f1
Timo Vesalainen <timo.vesalainen@iki.fi>
2015-12-30 03:44:48 -0300

    dev

--- src/main/java/org/vesalainen/math/AbstractPoint.java
+++ src/main/java/org/vesalainen/math/AbstractPoint.java
@@ -73,5 +73,37 @@ public class AbstractPoint implements Point, Serializable
     {
         this.y = y;
     }
+
+    @Override
+    public int hashCode()
+    {
+        int hash = 3;
+        hash = 97 * hash + (int) (Double.doubleToLongBits(this.x) ^ (Double.doubleToLongBits(this.x) >>> 32));
+        hash = 97 * hash + (int) (Double.doubleToLongBits(this.y) ^ (Double.doubleToLongBits(this.y) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (obj == null)
+        {
+            return false;
+        }
+        if (getClass() != obj.getClass())
+        {
+            return false;
+        }
+        final AbstractPoint other = (AbstractPoint) obj;
+        if (Double.doubleToLongBits(this.x) != Double.doubleToLongBits(other.x))
+        {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.y) != Double.doubleToLongBits(other.y))
+        {
+            return false;
+        }
+        return true;
+    }
     
 }
