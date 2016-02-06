c0b7029d45f33d1b288920f0dabff5ea8d3e6432
Timo Vesalainen <timo.vesalainen@iki.fi>
2016-02-06 12:34:04 -0400

    fix

--- src/main/java/org/vesalainen/util/HashMapList.java
+++ src/main/java/org/vesalainen/util/HashMapList.java
@@ -23,7 +23,6 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * HashMap and ArrayList based implementation of MapList
