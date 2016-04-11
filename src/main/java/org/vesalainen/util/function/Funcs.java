ee685bc771fdb9345e7eb839949cc19642d85c51
Timo Vesalainen <timo.vesalainen@iki.fi>
2016-04-11 07:40:02 -0400

    Add Funcs class for common method references.

---
+++ src/main/java/org/vesalainen/util/function/Funcs.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2016 tkv
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.vesalainen.util.function;
+
+/**
+ *
+ * @author tkv
+ */
+public class Funcs
+{
+    public static boolean same(boolean arg)
+    {
+        return arg;
+    }
+    public static byte same(byte arg)
+    {
+        return arg;
+    }
+    public static char same(char arg)
+    {
+        return arg;
+    }
+    public static short same(short arg)
+    {
+        return arg;
+    }
+    public int same(int arg)
+    {
+        return arg;
+    }
+    public long same(long arg)
+    {
+        return arg;
+    }
+    public float same(float arg)
+    {
+        return arg;
+    }
+    public double same(double arg)
+    {
+        return arg;
+    }
+    public Object same(Object arg)
+    {
+        return arg;
+    }
+}
