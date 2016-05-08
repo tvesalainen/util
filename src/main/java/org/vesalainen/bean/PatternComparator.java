7781e8c645886c21aef9f1c1bc80f924f783bb3a
Timo Vesalainen <timo.vesalainen@iki.fi>
2016-05-08 12:41:22 -0400

    Add PatternComparator

---
+++ src/main/java/org/vesalainen/bean/PatternComparator.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2016 tkv
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.vesalainen.bean;
+
+import java.util.Comparator;
+
+/**
+ * A comparator that puts patterns in following order: add, assign, normal and remove.
+ * Patterns which have same category are sorted in natural order. This means that
+ * this comparator is consistent with equals.
+ * @author tkv
+ */
+public class PatternComparator implements Comparator<String>
+{
+
+    @Override
+    public int compare(String o1, String o2)
+    {
+        int t1 = type(o1);
+        int t2 = type(o2);
+        if (t1 == t2)
+        {
+            return o1.compareTo(o2);
+        }
+        else
+        {
+            return t1 - t2;
+        }
+    }
+    
+    private int type(String p)
+    {
+        if (BeanHelper.isAdd(p))
+        {
+            return 0;
+        }
+        if (BeanHelper.isAssign(p))
+        {
+            return 1;
+        }
+        if (BeanHelper.isRemove(p))
+        {
+            return 3;
+        }
+        return 2;
+    }
+}
---
+++ src/test/java/org/vesalainen/bean/PatternComparatorTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2016 tkv
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.vesalainen.bean;
+
+import java.util.Arrays;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author tkv
+ */
+public class PatternComparatorTest
+{
+    
+    public PatternComparatorTest()
+    {
+    }
+
+    @Test
+    public void test1()
+    {
+        PatternComparator pc = new PatternComparator();
+        String[] sa = new String[] 
+        {
+            "f1",
+            "b#",
+            "b=hint",
+            "f2",
+            "a+hint",
+            "f3",
+        };
+        Arrays.sort(sa, pc);
+        assertEquals("a+hint", sa[0]);
+        assertEquals("b=hint", sa[1]);
+        assertEquals("f1", sa[2]);
+        assertEquals("f2", sa[3]);
+        assertEquals("f3", sa[4]);
+        assertEquals("b#", sa[5]);
+    }
+    
+}
