2e6128823fd297846adbb4ff9c0ae9082f2c3902
Timo Vesalainen <timo.vesalainen@iki.fi>
2017-07-23 11:35:36 -0400

    Added JarBuilder

---
+++ util/src/main/java/org/vesalainen/util/jar/JarBuilder.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2017 tkv
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.vesalainen.util.jar;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.Closeable;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.jar.JarEntry;
+import java.util.jar.JarOutputStream;
+
+/**
+ * Helper for creating jar files
+ * @author tkv
+ */
+public class JarBuilder implements Closeable
+{
+    private JarOutputStream jar;
+
+    public JarBuilder(File file) throws IOException
+    {
+        this(new FileOutputStream(file));
+    }
+
+    public JarBuilder(OutputStream os) throws IOException
+    {
+        this(new BufferedOutputStream(os));
+    }
+
+    public JarBuilder(BufferedOutputStream out) throws IOException
+    {
+        this.jar = new JarOutputStream(out);
+    }
+    
+    public void addEntry(String name, byte[] data) throws IOException
+    {
+        jar.putNextEntry(new JarEntry(name));
+        jar.write(data);
+    }
+    
+    public void addEntry(String name, File file) throws IOException
+    {
+        addEntry(name, new FileInputStream(file));
+    }
+    
+    public void addEntry(String name, InputStream is) throws IOException
+    {
+        addEntry(name, new BufferedInputStream(is));
+    }
+    
+    public void addEntry(String name, BufferedInputStream is) throws IOException
+    {
+        jar.putNextEntry(new JarEntry(name));
+        byte[] buf = new byte[4096];
+        int rc = is.read(buf);
+        while (rc != -1)
+        {
+            jar.write(buf, 0, rc);
+            rc = is.read(buf);
+        }
+    }
+    
+    @Override
+    public void close() throws IOException
+    {
+        jar.close();
+    }
+    
+}
---
+++ util/src/test/java/org/vesalainen/util/jar/JarBuilderTest.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2017 tkv
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.vesalainen.util.jar;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.jar.JarFile;
+import org.junit.After;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.vesalainen.nio.FileUtil;
+
+/**
+ *
+ * @author tkv
+ */
+public class JarBuilderTest
+{
+    static final File LOCAL = new File("jarTest");
+    
+    public JarBuilderTest()
+    {
+    }
+
+    @Before
+    public void before() throws IOException
+    {
+        Files.createDirectories(LOCAL.toPath());
+    }
+    @After
+    public void after() throws IOException
+    {
+        FileUtil.deleteDirectory(LOCAL);
+    }
+    
+    @Test
+    public void test1() throws IOException
+    {
+        File file = new File(LOCAL, "test.jar");
+        try (JarBuilder jar = new JarBuilder(file);
+                InputStream is = JarBuilderTest.class.getResourceAsStream("/lines_ansi.txt"))
+        {
+            jar.addEntry("lines_ansi.txt", is);
+            jar.addEntry("text", "qwerty".getBytes());
+        }
+        try (JarFile jf = new JarFile(file))
+        {
+            assertNotNull(jf.getJarEntry("lines_ansi.txt"));
+            assertNotNull(jf.getJarEntry("text"));
+        }
+    }
+    
+}
