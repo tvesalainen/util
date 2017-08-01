0b4520ca1554a8cbc8914da0c618164ebddb611a
Timo Vesalainen <timo.vesalainen@iki.fi>
2017-08-01 11:12:53 -0400

    doc/test

--- util/src/main/java/org/vesalainen/nio/FilterByteBuffer.java
+++ util/src/main/java/org/vesalainen/nio/FilterByteBuffer.java
@@ -18,6 +18,7 @@ package org.vesalainen.nio;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -28,15 +29,21 @@ import java.util.function.Function;
  * FilterByteBuffer enables filtered I/O to underlying ByteBuffer with 
  * ByteBuffer like API. Byte order is BIG_ENDIAN.
  * <p>
+ * Note! Get methods throw EOFException and put (flush) methods throw 
+ * BufferOverflowException when ByteBuffer remaining is not enough.
+ * <p>
  * Usage example: Read/Write GZIP content to/from FileChannel using mapped ByteBuffer.
  * 
  * @author tkv
+ * @see java.io.DataInputStream
+ * @see java.io.DataOutputStream
  */
 public class FilterByteBuffer implements AutoCloseable
 {
     private ByteBuffer bb;
-    DataInputStream in;
-    DataOutputStream out;
+    private DataInputStream in;
+    private DataOutputStream out;
+    private int position;
 
     /**
      * Creates new FilterByteBuffer over bb 
@@ -56,142 +63,347 @@ public class FilterByteBuffer implements AutoCloseable
             this.out = new DataOutputStream(fout.apply(new BBOut()));
         }
     }
-    
+    /**
+     * Returns current position. Position starts at 0. Every get and put method
+     * will increase position.
+     * @return 
+     */
     public int position()
     {
-        return bb.position();
+        return position;
     }
-           
+    /**
+     * Set's position to given int. Doesn't do anything else!
+     * @param position 
+     */
+    public void position(int position)
+    {
+        this.position = position;
+    }
+    /**
+     * Puts skip number of 0 bytes to output.
+     * @param skip
+     * @throws IOException 
+     */
+    public void skipOutput(int skip) throws IOException
+    {
+        for (int ii=0;ii<skip;ii++)
+        {
+            put((byte)0);
+        }
+    }
+    /**
+     * Skips skip number of bytes from input.
+     * @param skip
+     * @throws IOException 
+     */
+    public void skipInput(int skip) throws IOException
+    {
+        in.skip(skip);
+        position += skip;
+    }
+    /**
+     * Skips output so that position % align == 0
+     * @param align
+     * @throws IOException 
+     */
+    public void alignOutput(int align) throws IOException
+    {
+        int mod = position % align;
+        if (mod > 0)
+        {
+            skipOutput(align - mod);
+        }
+    }
+    /**
+     * Skips output so that position % align == 0
+     * @param align
+     * @throws IOException 
+     */
+    public void alignInput(int align) throws IOException
+    {
+        int mod = position % align;
+        if (mod > 0)
+        {
+            skipInput(align - mod);
+        }
+    }
+    /**
+     * Flushes output to ByteBuffer.
+     * @throws IOException 
+     * @throws BufferOverflowException If not enough room in ByteBuffer.
+     */
     public void flush() throws IOException
     {
         checkOut();
         out.flush();
     }
+    /**
+     * Returns next byte.
+     * @return
+     * @throws IOException
+     * @throws EOFException If get request couldn't be filled. 
+     */
     public byte get() throws IOException
     {
         checkIn();
+        position++;
         return in.readByte();
     }
-
+    /**
+     * Puts byte.
+     * @param b
+     * @return
+     * @throws IOException 
+     * @throws BufferOverflowException If not enough room in ByteBuffer.
+     */
     public FilterByteBuffer put(byte b) throws IOException
     {
         checkOut();
         out.writeByte(b);
+        position++;
         return this;
     }
+    /**
+     * Tries to read length bytes to dst. 
+     * @param dst
+     * @param offset
+     * @param length
+     * @return
+     * @throws IOException 
+     * @throws EOFException If get request couldn't be filled. 
+     */
     public FilterByteBuffer get(byte[] dst, int offset, int length) throws IOException
     {
         checkIn();
-        in.read(dst, offset, length);
+        int rc = in.read(dst, offset, length);
+        if (rc != length)
+        {
+            throw new EOFException();
+        }
+        position += length;
         return this;
     }
+    /**
+     * Tries to fill dst. 
+     * @param dst
+     * @return
+     * @throws IOException 
+     * @throws EOFException If get request couldn't be filled. 
+     */
     public FilterByteBuffer get(byte[] dst) throws IOException
     {
         checkIn();
-        in.read(dst);
+        int rc = in.read(dst);
+        if (rc != dst.length)
+        {
+            throw new EOFException();
+        }
+        position += dst.length;
         return this;
     }
+    /**
+     * Puts length bytes.
+     * @param src
+     * @param offset
+     * @param length
+     * @return
+     * @throws IOException 
+     * @throws BufferOverflowException If not enough room in ByteBuffer.
+     */
     public FilterByteBuffer put(byte[] src, int offset, int length) throws IOException
     {
         checkOut();
         out.write(src, offset, length);
+        position += length;
         return this;
     }
+    /**
+     * Puts whole dst.
+     * @param src
+     * @return
+     * @throws IOException 
+     * @throws BufferOverflowException If not enough room in ByteBuffer.
+     */
     public FilterByteBuffer put(byte[] src) throws IOException
     {
         checkOut();
         out.write(src);
+        position += src.length;
         return this;
     }
-        
+    /**
+     * Return 2 next bytes as char
+     * @return
+     * @throws IOException 
+     * @throws EOFException If get request couldn't be filled. 
+     */
     public char getChar() throws IOException
     {
         checkIn();
+        position += 2;
         return in.readChar();
     }
-
+    /**
+     * Puts char as 2 bytes.
+     * @param value
+     * @return
+     * @throws IOException 
+     * @throws BufferOverflowException If not enough room in ByteBuffer.
+     */
     public FilterByteBuffer putChar(char value) throws IOException
     {
         checkOut();
         out.writeChar(value);
+        position += 2;
         return this;
     }
-
+    /**
+     * Returns short as 2 bytes.
+     * @return
+     * @throws IOException 
+     * @throws EOFException If get request couldn't be filled. 
+     */
     public short getShort() throws IOException
     {
         checkIn();
+        position += 2;
         return in.readShort();
     }
-
+    /**
+     * Puts short as 2 bytes.
+     * @param value
+     * @return
+     * @throws IOException 
+     * @throws BufferOverflowException If not enough room in ByteBuffer.
+     */
     public FilterByteBuffer putShort(short value) throws IOException
     {
         checkOut();
         out.writeShort(value);
+        position += 2;
         return this;
     }
-
+    /**
+     * Returns int as 4 bytes.
+     * @return
+     * @throws IOException 
+     * @throws EOFException If get request couldn't be filled. 
+     */
     public int getInt() throws IOException
     {
         checkIn();
+        position += 4;
         return in.readInt();
     }
-
+    /**
+     * Puts int as 4 bytes.
+     * @param value
+     * @return
+     * @throws IOException 
+     * @throws BufferOverflowException If not enough room in ByteBuffer.
+     */
     public FilterByteBuffer putInt(int value) throws IOException
     {
         checkOut();
         out.writeInt(value);
+        position += 4;
         return this;
     }
-
+    /**
+     * Returns long as 8 bytes
+     * @return
+     * @throws IOException 
+     * @throws EOFException If get request couldn't be filled. 
+     */
     public long getLong() throws IOException
     {
         checkIn();
+        position += 8;
         return in.readLong();
     }
-
+    /**
+     * Puts long as 8 bytes.
+     * @param value
+     * @return
+     * @throws IOException 
+     * @throws BufferOverflowException If not enough room in ByteBuffer.
+     */
     public FilterByteBuffer putLong(long value) throws IOException
     {
         checkOut();
         out.writeLong(value);
+        position += 8;
         return this;
     }
-
+    /**
+     * Returns float as 4 bytes
+     * @return
+     * @throws IOException 
+     * @throws EOFException If get request couldn't be filled. 
+     */
     public float getFloat() throws IOException
     {
         checkIn();
+        position += 4;
         return in.readFloat();
     }
-
+    /**
+     * Puts float as 4 bytes.
+     * @param value
+     * @return
+     * @throws IOException 
+     * @throws BufferOverflowException If not enough room in ByteBuffer.
+     */
     public FilterByteBuffer putFloat(float value) throws IOException
     {
         checkOut();
         out.writeFloat(value);
+        position += 4;
         return this;
     }
-
+    /**
+     * Returns double as 8 bytes
+     * @return
+     * @throws IOException 
+     * @throws EOFException If get request couldn't be filled. 
+     */
     public double getDouble() throws IOException
     {
         checkIn();
+        position += 8;
         return in.readDouble();
     }
-
+    /**
+     * Puts double as 8 bytes.
+     * @param value
+     * @return
+     * @throws IOException 
+     * @throws BufferOverflowException If not enough room in ByteBuffer.
+     */
     public FilterByteBuffer putDouble(double value) throws IOException
     {
         checkOut();
         out.writeDouble(value);
+        position += 8;
         return this;
     }
-
+    /**
+     * Closes underlying streams.
+     * @throws Exception 
+     */
     @Override
     public void close() throws Exception
     {
         if (out != null)
         {
             out.close();
+            out = null;
         }
         if (in != null)
         {
             in.close();
+            in = null;
         }
     }
 
--- util/src/test/java/org/vesalainen/nio/FilterByteBufferTest.java
+++ util/src/test/java/org/vesalainen/nio/FilterByteBufferTest.java
@@ -18,7 +18,9 @@ package org.vesalainen.nio;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.EOFException;
 import java.io.IOException;
+import java.nio.BufferOverflowException;
 import java.nio.ByteBuffer;
 import static java.nio.charset.StandardCharsets.US_ASCII;
 import static org.junit.Assert.*;
@@ -40,30 +42,129 @@ public class FilterByteBufferTest
     {
         ByteBuffer bb = ByteBuffer.allocate(1024);
         FilterByteBuffer fbb = new FilterByteBuffer(bb, BufferedInputStream::new, BufferedOutputStream::new);
+        int ep = 0;
+        assertEquals(ep, fbb.position());
         fbb.put((byte)1);
+        ep++;
+        assertEquals(ep, fbb.position());
+        fbb.alignOutput(8);
+        ep+=7;
+        assertEquals(ep, fbb.position());
         fbb.put("qwerty".getBytes(US_ASCII));
+        ep+=6;
+        assertEquals(ep, fbb.position());
         fbb.put("asdfghjk".getBytes(US_ASCII), 2, 3);
+        ep+=3;
+        assertEquals(ep, fbb.position());
         fbb.putChar('a');
+        ep+=2;
+        assertEquals(ep, fbb.position());
         fbb.putDouble(123.456);
+        ep+=8;
+        assertEquals(ep, fbb.position());
         fbb.putFloat(1.23F);
+        ep+=4;
+        assertEquals(ep, fbb.position());
         fbb.putInt(123456);
+        ep+=4;
+        assertEquals(ep, fbb.position());
         fbb.putLong(987654321L);
+        ep+=8;
+        assertEquals(ep, fbb.position());
         fbb.putShort((short)123);
+        ep+=2;
+        assertEquals(ep, fbb.position());
         fbb.flush();
+        fbb.position(0);
+        ep = 0;
         
         bb.flip();
         assertEquals(1, fbb.get());
+        ep+=1;
+        assertEquals(ep, fbb.position());
+        fbb.alignInput(8);
+        ep+=7;
+        assertEquals(ep, fbb.position());
         byte[] buf = new byte[6];
         fbb.get(buf);
+        ep+=6;
+        assertEquals(ep, fbb.position());
         assertEquals("qwerty", new String(buf, US_ASCII));
         fbb.get(buf, 2, 3);
+        ep+=3;
+        assertEquals(ep, fbb.position());
         assertEquals("dfg", new String(buf, 2, 3, US_ASCII));
         assertEquals('a', fbb.getChar());
+        ep+=2;
+        assertEquals(ep, fbb.position());
         assertEquals(123.456, fbb.getDouble(), 1e-10);
+        ep+=8;
+        assertEquals(ep, fbb.position());
         assertEquals(1.23F, fbb.getFloat(), 1e-10);
+        ep+=4;
+        assertEquals(ep, fbb.position());
         assertEquals(123456, fbb.getInt());
+        ep+=4;
+        assertEquals(ep, fbb.position());
         assertEquals(987654321L, fbb.getLong());
+        ep+=8;
+        assertEquals(ep, fbb.position());
         assertEquals(123, fbb.getShort());
+        ep+=2;
+        assertEquals(ep, fbb.position());
     }
-    
+    @Test
+    public void testEOF() throws IOException
+    {
+        ByteBuffer bb = ByteBuffer.allocate(1024);
+        FilterByteBuffer fbb = new FilterByteBuffer(bb, BufferedInputStream::new, BufferedOutputStream::new);
+        fbb.put((byte)1);
+        fbb.flush();
+        bb.flip();
+        try
+        {
+            fbb.getShort();
+            fail("should throw EOFException");
+        }
+        catch (EOFException ex)
+        {
+            
+        }
+    }    
+    @Test
+    public void testUnderflow() throws IOException
+    {
+        ByteBuffer bb = ByteBuffer.allocate(1024);
+        FilterByteBuffer fbb = new FilterByteBuffer(bb, BufferedInputStream::new, BufferedOutputStream::new);
+        fbb.put((byte)1);
+        fbb.flush();
+        bb.flip();
+        
+        try
+        {
+            byte[] buf = new byte[10];
+            fbb.get(buf);
+            fail("should throw EOFException");
+        }
+        catch (EOFException ex)
+        {
+            
+        }
+    }
+    @Test
+    public void testOverflow() throws IOException
+    {
+        ByteBuffer bb = ByteBuffer.allocate(7);
+        FilterByteBuffer fbb = new FilterByteBuffer(bb, BufferedInputStream::new, BufferedOutputStream::new);
+        fbb.putDouble(123.456);
+        try
+        {
+            fbb.flush();
+            fail("should throw BufferOverflowException");
+        }
+        catch (BufferOverflowException ex)
+        {
+            
+        }
+    }
 }
