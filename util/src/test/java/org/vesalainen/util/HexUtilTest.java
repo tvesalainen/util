a149f2c0b3f9cb7f4da73f999f85c3c54467b6e4
Timo Vesalainen <timo.vesalainen@iki.fi>
2017-08-05 16:11:04 -0400

    Added HexUtil

---
+++ util/src/test/java/org/vesalainen/util/HexUtilTest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2017 tkv
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.vesalainen.util;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author tkv
+ */
+public class HexUtilTest
+{
+    
+    public HexUtilTest()
+    {
+    }
+
+    @Test
+    public void test1()
+    {
+        byte[] exp = new byte[]{(byte)0x0b, (byte)0xcd, (byte)0xef, (byte)0x34};
+        assertEquals("0BCDEF34", HexUtil.toString(exp));
+        assertArrayEquals(exp, HexUtil.fromString("0BCDEF34"));
+    }
+    
+}
