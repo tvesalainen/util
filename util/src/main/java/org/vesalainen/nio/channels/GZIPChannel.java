ac34b6f0ba77ec3beab6cda622cc80d2a4ac8f71
Timo Vesalainen <timo.vesalainen@iki.fi>
2017-09-01 15:07:03 -0400

    Tested GZIPChannel

--- util/src/main/java/org/vesalainen/nio/channels/GZIPChannel.java
+++ util/src/main/java/org/vesalainen/nio/channels/GZIPChannel.java
@@ -18,29 +18,38 @@ package org.vesalainen.nio.channels;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.ByteBuffer;
 import static java.nio.ByteOrder.*;
 import java.nio.channels.ByteChannel;
+import java.nio.channels.Channels;
 import java.nio.channels.ClosedChannelException;
 import java.nio.channels.FileChannel;
 import java.nio.channels.NonReadableChannelException;
 import java.nio.channels.NonWritableChannelException;
 import static java.nio.charset.StandardCharsets.ISO_8859_1;
-import static java.nio.charset.StandardCharsets.US_ASCII;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
 import java.nio.file.OpenOption;
 import java.nio.file.Path;
 import static java.nio.file.StandardOpenOption.*;
 import java.nio.file.attribute.FileAttribute;
+import java.nio.file.attribute.FileTime;
+import java.time.Instant;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.zip.CRC32;
 import java.util.zip.DataFormatException;
 import java.util.zip.Deflater;
 import static java.util.zip.Deflater.*;
 import java.util.zip.Inflater;
+import org.vesalainen.util.OperatingSystem;
 
 /**
  *
@@ -49,7 +58,7 @@ import java.util.zip.Inflater;
 public class GZIPChannel implements ByteChannel
 {
     private static final int BUF_SIZE = 4096;
-    public final static int MAGIC = 0x1f8b;
+    public final static short MAGIC = (short)0x8b1f;
     private final static int FTEXT = 1;
     private final static int FHCRC = 2;
     private final static int FEXTRA = 4;
@@ -65,6 +74,8 @@ public class GZIPChannel implements ByteChannel
     private byte[] uncompBuf = new byte[BUF_SIZE];
     private final Path path;
     private String comment;
+    private CRC32 crc32 = new CRC32();
+    private FileTime lastModified;
 
     public GZIPChannel(Path path, OpenOption... options) throws IOException
     {
@@ -89,8 +100,45 @@ public class GZIPChannel implements ByteChannel
         {
             this.options.add(READ);
         }
+        compBuf.order(LITTLE_ENDIAN);
+        compBuf.flip();
     }
-
+    public void compress(Stream<Path> paths) throws IOException
+    {
+        boolean first = true;
+        Iterator<Path> it = paths.iterator();
+        while (it.hasNext())
+        {
+            Path p = it.next();
+            if (Files.isRegularFile(path))
+            {
+                setFilename(p.getFileName().toString());
+                setLastModified(Files.getLastModifiedTime(p));
+                if (!first)
+                {
+                    nextOutput();
+                }
+                first = false;
+                OutputStream os = Channels.newOutputStream(this);
+                Files.copy(p, os);
+            }
+            else
+            {
+                throw new IOException(p+" is not a regular file");
+            }
+        }
+    }
+    public void extractAll(Path targetDir, CopyOption... options) throws IOException
+    {
+        ensureReading();
+        do
+        {
+            Path p = targetDir.resolve(filename);
+            InputStream is = Channels.newInputStream(this);
+            Files.copy(is, p, options);
+            Files.setLastModifiedTime(p, lastModified);
+        } while (nextInput());
+    }
     @Override
     public int read(ByteBuffer dst) throws IOException
     {
@@ -101,15 +149,21 @@ public class GZIPChannel implements ByteChannel
             int rc;
             while ((rc = inflater.inflate(uncompBuf, 0, length)) == 0)
             {
-                if (inflater.finished() || inflater.needsDictionary())
+                if (inflater.needsDictionary())
                 {
+                    throw new IOException("inflater needs dictionary");
+                }
+                if (inflater.finished())
+                {
+                    readTrailer();
                     return -1;
                 }
                 if (inflater.needsInput())
                 {
-                    fill();
+                    fillInflater();
                 }
             }
+            crc32.update(uncompBuf, 0, rc);
             dst.put(uncompBuf, 0, rc);
             return rc;
         }
@@ -118,7 +172,7 @@ public class GZIPChannel implements ByteChannel
             throw new IOException(ex);
         }
     }
-    private void fill() throws IOException
+    private void fillInflater() throws IOException
     {
         if (!compBuf.hasRemaining())
         {
@@ -126,7 +180,7 @@ public class GZIPChannel implements ByteChannel
             channel.read(compBuf);
             compBuf.flip();
         }
-        inflater.setInput(compBuf.array(), compBuf.position(), compBuf.limit());
+        inflater.setInput(compBuf.array(), compBuf.position(), compBuf.remaining());
         compBuf.position(compBuf.limit());
     }
 
@@ -134,7 +188,25 @@ public class GZIPChannel implements ByteChannel
     public int write(ByteBuffer src) throws IOException
     {
         ensureWriting();
-        throw new UnsupportedOperationException();
+        int count = src.remaining();
+        while (src.hasRemaining())
+        {
+            if (deflater.needsInput())
+            {
+                int len = Math.min(src.remaining(), uncompBuf.length);
+                src.get(uncompBuf, 0, len);
+                deflater.setInput(uncompBuf, 0, len);
+                crc32.update(uncompBuf, 0, len);
+            }
+            compBuf.clear();
+            int rc = deflater.deflate(compBuf.array());
+            if (rc > 0)
+            {
+                compBuf.position(rc);
+                channel.write(compBuf);
+            }
+        }
+        return count;
     }
 
     @Override
@@ -146,7 +218,14 @@ public class GZIPChannel implements ByteChannel
     @Override
     public void close() throws IOException
     {
-        channel.close();
+        if (channel.isOpen())
+        {
+            if (deflater != null)
+            {
+                writeTrailer();
+            }
+            channel.close();
+        }
     }
 
     private void ensureReading() throws IOException
@@ -185,7 +264,9 @@ public class GZIPChannel implements ByteChannel
 
     private void readHeader() throws IOException
     {
-        compBuf.order(BIG_ENDIAN);
+        filename = null;
+        comment = null;
+        compBuf.compact();
         channel.read(compBuf);
         compBuf.flip();
         short magic = compBuf.getShort();
@@ -200,6 +281,7 @@ public class GZIPChannel implements ByteChannel
         }
         byte flag = compBuf.get();
         int mtime = compBuf.getInt();
+        lastModified = FileTime.from(mtime, TimeUnit.SECONDS);
         byte xfl = compBuf.get();
         byte os = compBuf.get();
         if ((flag & FEXTRA) != 0)
@@ -217,13 +299,70 @@ public class GZIPChannel implements ByteChannel
         }
         if ((flag & FHCRC) != 0)
         {
-            short crc = compBuf.getShort();
+            ByteBuffer duplicate = compBuf.duplicate();
+            duplicate.flip();
+            CRC32 crc32 = new CRC32();
+            crc32.update(duplicate);
+            int value = (int) (crc32.getValue() & 0xffff);
+            int crc16 = (compBuf.getShort() & 0xffff);
+            if (crc16 != value)
+            {
+                throw new IOException("CRC16 mismatch");
+            }
         }
     }
 
-    private void writeHeader()
+    private void writeHeader() throws IOException
     {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        compBuf.clear();
+        compBuf.putShort(MAGIC);
+        compBuf.put((byte)8);
+        byte flags = FHCRC;
+        if (filename != null)
+        {
+            flags |= FNAME;
+        }
+        if (comment != null)
+        {
+            flags |= FCOMMENT;
+        }
+        if (lastModified == null)
+        {
+            lastModified = FileTime.from(Instant.now());
+        }
+        compBuf.put(flags);
+        compBuf.putInt((int) lastModified.to(TimeUnit.SECONDS));
+        lastModified = null;
+        compBuf.put((byte)2);   // compressor used maximum compression, slowest algorithm
+        switch (OperatingSystem.getOperatingSystem())
+        {
+            case Linux:
+                compBuf.put((byte)3);   // unix
+                break;
+            case Windows:
+                compBuf.put((byte)11);  // NTFS filesystem (NT)
+                break;
+            default:
+                compBuf.put((byte)255);  // unknown
+                break;
+        }
+        if (filename != null)
+        {
+            writeString(compBuf, filename);
+        }
+        if (comment != null)
+        {
+            writeString(compBuf, comment);
+        }
+        filename = null;
+        comment = null;
+        ByteBuffer duplicate = compBuf.duplicate();
+        duplicate.flip();
+        CRC32 crc = new CRC32();
+        crc.update(duplicate);
+        compBuf.putShort((short) (crc.getValue() & 0xffff));
+        compBuf.flip();
+        channel.write(compBuf);
     }
     
     private String readString(ByteBuffer bb)
@@ -236,6 +375,119 @@ public class GZIPChannel implements ByteChannel
             cc = bb.get() & 0xff;
         }
         return new String(bais.toByteArray(), ISO_8859_1);
+    }
+
+    private void readTrailer() throws IOException
+    {
+        compBuf.position(compBuf.position() - inflater.getRemaining());
+        compBuf.compact();
+        channel.read(compBuf);
+        compBuf.flip();
+        int crc = compBuf.getInt();
+        long value = crc32.getValue();
+        if (crc != (value & 0xffffffff))
+        {
+            throw new IOException("CRC32 mismatch");
+        }
+        int isize = compBuf.getInt();
+        if (isize != (inflater.getBytesWritten() & 0xffffffff))
+        {
+            throw new IOException("Size mismatch");
+        }
+    }
+    /**
+     * After read returns -1 call nextInput to advance to nextInput file.
+     * @return true if nextInput file available.
+     * @throws IOException 
+     */
+    public boolean nextInput() throws IOException
+    {
+        if (!options.contains(READ) && !inflater.finished())
+        {
+            throw new IllegalStateException("should be called after read returns -1");
+        }
+        if (!compBuf.hasRemaining())
+        {
+            compBuf.clear();
+            int rc = channel.read(compBuf);
+            if (rc == -1)
+            {
+                return false;
+            }
+            compBuf.flip();
+        }
+        readHeader();
+        inflater.reset();
+        crc32.reset();
+        return true;
+    }
+    public void nextOutput() throws IOException
+    {
+        writeTrailer();
+        writeHeader();
+    }
+    /**
+     * Returns original filename without path or null name doesn't exist.
+     * @return 
+     */
+    public String getFilename()
+    {
+        return filename;
+    }
+    /**
+     * Returns comment or null.
+     * @return 
+     */
+    public String getComment()
+    {
+        return comment;
+    }
+    /**
+     * Sets original filename for compressed file.
+     * Set before first write.
+     * @param filename 
+     */
+    public void setFilename(String filename)
+    {
+        this.filename = filename;
+    }
+    /**
+     * Sets comment for compressed file.
+     * Set before first write.
+     * @param comment 
+     */
+    public void setComment(String comment)
+    {
+        this.comment = comment;
+    }
+    /**
+     * Sets last modified time for compressed file.
+     * Set before first write.
+     * @param lastModified 
+     */
+    public void setLastModified(FileTime lastModified)
+    {
+        this.lastModified = lastModified;
+    }
+
+    private void writeString(ByteBuffer bb, String text)
+    {
+        bb.put(text.getBytes(ISO_8859_1));
+        bb.put((byte)0);
+    }
+
+    private void writeTrailer() throws IOException
+    {
+        deflater.finish();
+        compBuf.clear();
+        int rc = deflater.deflate(compBuf.array());
+        compBuf.position(rc);
+        compBuf.putInt((int) (crc32.getValue() & 0xffffffffL));
+        compBuf.putInt((int) (deflater.getBytesRead() & 0xffffffffL));
+        compBuf.flip();
+        channel.write(compBuf);
+        crc32.reset();
+        deflater.reset();
     }
 
 }
--- util/src/test/java/org/vesalainen/nio/channels/GZIPChannelTest.java
+++ util/src/test/java/org/vesalainen/nio/channels/GZIPChannelTest.java
@@ -21,7 +21,10 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.ByteBuffer;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 import static java.nio.file.StandardOpenOption.*;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -38,16 +41,56 @@ public class GZIPChannelTest
     }
 
     @Test
-    public void test1() throws URISyntaxException, IOException
+    public void testRead() throws URISyntaxException, IOException
     {
-        URL url = GZIPChannelTest.class.getResource("/router2.gz");
+        URL url = GZIPChannelTest.class.getResource("/test.gz");
         File file = new File(url.toURI());
         Path path = file.toPath();
         ByteBuffer bb = ByteBuffer.allocate(4096);
         try (GZIPChannel ch = new GZIPChannel(path, READ))
         {
+            ch.read(bb);    // reads data
+            assertEquals("router2.xml", ch.getFilename());
+            ch.read(bb);    // reads -1
+            assertTrue(ch.nextInput());
+            assertEquals("txt", ch.getFilename());
             ch.read(bb);
             ch.read(bb);
+            assertFalse(ch.nextInput());
+        }
+    }
+    //@Test
+    public void testExtract() throws URISyntaxException, IOException
+    {
+        URL url = GZIPChannelTest.class.getResource("/test.gz");
+        File file = new File(url.toURI());
+        Path path = file.toPath();
+        ByteBuffer bb = ByteBuffer.allocate(4096);
+        try (GZIPChannel ch = new GZIPChannel(path, READ))
+        {
+            Path dir = Paths.get(".");
+            ch.extractAll(dir, REPLACE_EXISTING);
+        }
+    }
+    //@Test
+    public void testCompress() throws URISyntaxException, IOException
+    {
+        Path path = Paths.get("z:\\testi.gz");
+        try (GZIPChannel ch = new GZIPChannel(path, CREATE,WRITE))
+        {
+            Path dir = Paths.get(".");
+            ch.compress(Files.list(dir).filter((p)->Files.isRegularFile(p)));
+        }
+    }
+    //@Test
+    public void testExtract2() throws URISyntaxException, IOException
+    {
+        Path path = Paths.get("z:\\testi.gz");
+        ByteBuffer bb = ByteBuffer.allocate(4096);
+        try (GZIPChannel ch = new GZIPChannel(path, READ))
+        {
+            Path dir = Paths.get("z:\\");
+            ch.extractAll(dir, REPLACE_EXISTING);
         }
     }
     
