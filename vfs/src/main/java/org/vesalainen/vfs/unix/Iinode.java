921ae46c300e1c43795457434692b76d21f4e4ee
Timo Vesalainen <timo.vesalainen@iki.fi>
2017-09-10 12:00:38 -0400

    dev

---
+++ vfs/src/main/java/org/vesalainen/vfs/unix/Iinode.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2017 Timo Vesalainen <timo.vesalainen@iki.fi>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.vesalainen.vfs.unix;
+
+/**
+ *
+ * @author Timo Vesalainen <timo.vesalainen@iki.fi>
+ */
+public class Iinode
+{
+    private long devminor;
+    private long inode;
+
+    public Iinode(long devminor, long inode)
+    {
+        this.devminor = devminor;
+        this.inode = inode;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int hash = 7;
+        hash = 73 * hash + (int) (this.devminor ^ (this.devminor >>> 32));
+        hash = 73 * hash + (int) (this.inode ^ (this.inode >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+        {
+            return true;
+        }
+        if (obj == null)
+        {
+            return false;
+        }
+        if (getClass() != obj.getClass())
+        {
+            return false;
+        }
+        final Iinode other = (Iinode) obj;
+        if (this.devminor != other.devminor)
+        {
+            return false;
+        }
+        if (this.inode != other.inode)
+        {
+            return false;
+        }
+        return true;
+    }
+    
+}
