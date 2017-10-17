dfb08c8b457b3729226986c77f404975399b8879
Timo Vesalainen <timo.vesalainen@iki.fi>
2017-10-17 16:13:54 -0400

    Added SimpleNotificationEmitter

---
+++ util/src/main/java/org/vesalainen/management/SimpleNotificationEmitter.java
@@ -0,0 +1,197 @@
+/*
+ * Copyright (C) 2017 Timo Vesalainen <timo.vesalainen@iki.fi>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.vesalainen.management;
+
+import java.util.Objects;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Supplier;
+import java.util.logging.Level;
+import javax.management.ListenerNotFoundException;
+import javax.management.MBeanNotificationInfo;
+import javax.management.Notification;
+import javax.management.NotificationEmitter;
+import javax.management.NotificationFilter;
+import javax.management.NotificationListener;
+import javax.management.ObjectName;
+import org.vesalainen.util.HashMapList;
+import org.vesalainen.util.MapList;
+import org.vesalainen.util.logging.JavaLogging;
+
+/**
+ * NotificationEmitter implementation which tries to minimize cpu load in
+ * transmitting part in particular if there are no listeners.
+ * @author Timo Vesalainen <timo.vesalainen@iki.fi>
+ */
+public class SimpleNotificationEmitter<T> implements NotificationEmitter
+{
+    private MapList<NotificationListener,ListenerWrapper> map = new HashMapList<>();
+    private String type;
+    private ObjectName source;
+    private MBeanNotificationInfo[] infos;
+    private Executor executor;
+    private AtomicLong sequenceNumber = new AtomicLong();
+    /**
+     * Creates NotificationEmitterImpl with gives infos
+     * @param infos 
+     */
+    public SimpleNotificationEmitter(String type, ObjectName source, MBeanNotificationInfo... infos)
+    {
+        this(Executors.newCachedThreadPool(), type, source, infos);
+    }
+    /**
+     * Creates NotificationEmitterImpl with gives executer and infos
+     * @param executor
+     * @param infos 
+     */
+    public SimpleNotificationEmitter(Executor executor, String type, ObjectName source, MBeanNotificationInfo... infos)
+    {
+        this.executor = executor;
+        this.type = type;
+        this.source = source;
+        this.infos = infos;
+    }
+    /**
+     * Send notification. supplier is called only if there are listeners
+     * @param notificationSupplier 
+     */
+    public synchronized void sendNotification(Supplier<String> textSupplier, Supplier<T> userDataSupplier)
+    {
+        if (!map.isEmpty())
+        {
+            sendNotification(textSupplier.get(), userDataSupplier.get());
+        }
+    }
+    /**
+     * Send notification.
+     * @param notification 
+     */
+    public synchronized void sendNotification(String text, T userData)
+    {
+        map.allValues()
+                .forEach((ListenerWrapper w)->executor.execute(()->w.sendNotification(text, userData, System.currentTimeMillis())));
+    }
+    @Override
+    public synchronized void removeNotificationListener(NotificationListener listener, NotificationFilter filter, Object handback) throws ListenerNotFoundException
+    {
+        ListenerWrapper wrap = new ListenerWrapper(listener, filter, handback);
+        if (!map.removeItem(listener, wrap))
+        {
+            throw new ListenerNotFoundException();
+        }
+    }
+
+    @Override
+    public synchronized void addNotificationListener(NotificationListener listener, NotificationFilter filter, Object handback) throws IllegalArgumentException
+    {
+        if (listener == null)
+        {
+            throw new IllegalArgumentException();
+        }
+        ListenerWrapper wrap = new ListenerWrapper(listener, filter, handback);
+        map.add(listener, wrap);
+    }
+
+    @Override
+    public synchronized void removeNotificationListener(NotificationListener listener) throws ListenerNotFoundException
+    {
+        if (map.remove(listener) == null)
+        {
+            throw new ListenerNotFoundException();
+        }
+    }
+
+    @Override
+    public MBeanNotificationInfo[] getNotificationInfo()
+    {
+        return infos;
+    }
+
+    private class ListenerWrapper
+    {
+        private NotificationListener listener;
+        private NotificationFilter filter;
+        private Object handback;
+
+        public ListenerWrapper(NotificationListener listener, NotificationFilter filter, Object handback)
+        {
+            this.listener = listener;
+            this.filter = filter;
+            this.handback = handback;
+        }
+
+        public void sendNotification(String text, T userData, long timeStamp)
+        {
+            Notification notification = new Notification(type, source, sequenceNumber.incrementAndGet(), timeStamp, text);
+            notification.setUserData(userData);
+            if (filter == null || filter.isNotificationEnabled(notification))
+            {
+                try
+                {
+                    listener.handleNotification(notification, handback);
+                }
+                catch (Exception ex)
+                {
+                    JavaLogging.getLogger(SimpleNotificationEmitter.class).log(Level.SEVERE, ex, "notify %s", ex.getMessage());
+                }
+            }
+        }
+        
+        @Override
+        public int hashCode()
+        {
+            int hash = 5;
+            hash = 29 * hash + Objects.hashCode(this.listener);
+            hash = 29 * hash + Objects.hashCode(this.filter);
+            hash = 29 * hash + Objects.hashCode(this.handback);
+            return hash;
+        }
+
+        @Override
+        public boolean equals(Object obj)
+        {
+            if (this == obj)
+            {
+                return true;
+            }
+            if (obj == null)
+            {
+                return false;
+            }
+            if (getClass() != obj.getClass())
+            {
+                return false;
+            }
+            final ListenerWrapper other = (ListenerWrapper) obj;
+            if (!Objects.equals(this.listener, other.listener))
+            {
+                return false;
+            }
+            if (!Objects.equals(this.filter, other.filter))
+            {
+                return false;
+            }
+            if (!Objects.equals(this.handback, other.handback))
+            {
+                return false;
+            }
+            return true;
+        }
+        
+    }
+}
