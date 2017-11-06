c87614dda057fc4b1cd057233f4d8ab48012ec3a
Timo Vesalainen <timo.vesalainen@iki.fi>
2017-11-06 13:36:19 -0400

    Changed read from greedy to reluctant

--- util/src/main/java/org/vesalainen/nio/channels/FilterChannel.java
+++ util/src/main/java/org/vesalainen/nio/channels/FilterChannel.java
@@ -223,30 +223,21 @@ public class FilterChannel implements SeekableByteChannel, GatheringSupport, Sca
         readLock();
         try
         {
-            int res = 0;
-            while (dst.hasRemaining())
+            if (length == 0)
             {
-                if (length == 0)
+                length = in.read(buf);
+                if (length == -1)
                 {
-                    length = in.read(buf);
-                    if (length == -1)
-                    {
-                        length = 0;
-                        if (res > 0)
-                        {
-                            return res;
-                        }
-                        return -1;
-                    }
-                    offset = 0;
+                    length = 0;
+                    return -1;
                 }
-                int count = ByteBuffers.move(buf, offset, length, dst);
-                offset += count;
-                length -= count;
-                position += count;
-                res += count;
+                offset = 0;
             }
-            return res;
+            int count = ByteBuffers.move(buf, offset, length, dst);
+            offset += count;
+            length -= count;
+            position += count;
+            return count;
         }
         finally
         {
