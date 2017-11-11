e745eea5ab9800a9b490340555f7b2a650be6d69
Timo Vesalainen <timo.vesalainen@iki.fi>
2017-11-11 09:36:46 -0400

    Added Range interface

---
+++ util/src/main/java/org/vesalainen/util/Range.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (C) 2017 Timo Vesalainen <timo.vesalainen@iki.fi>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.vesalainen.util;
+
+
+/**
+ *
+ * @author Timo Vesalainen <timo.vesalainen@iki.fi>
+ */
+public interface Range extends Comparable<Range>
+{
+
+    /**
+     * Returns true is n is inside this range
+     * @param n
+     * @return
+     */
+    default boolean accept(int n)
+    {
+        return (getFrom() <= n) && (getTo() > n);
+    }
+
+    @Override
+    default int compareTo(Range o)
+    {
+        if (getFrom() != o.getFrom())
+        {
+            return getFrom() - o.getFrom();
+        }
+        else
+        {
+            return o.getTo() - getTo();
+        }
+    }
+
+    /**
+     * Returns true if argument is inside this range
+     * @param r
+     * @return
+     */
+    default boolean contains(Range r)
+    {
+        if (r != null)
+        {
+            return (getFrom() <= r.getFrom()) && (getTo() >= r.getTo());
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    /**
+     * Returns true if from - to is inside this range
+     * @param from
+     * @param to
+     * @return
+     */
+    default boolean contains(int from, int to)
+    {
+        return (getFrom() <= from) && (getTo() >= to);
+    }
+
+    /**
+     * Returns true is n is inside this range
+     * @param n
+     * @return
+     */
+    default boolean contains(int n)
+    {
+        return accept(n);
+    }
+
+    /**
+     * Returns the lowest character value
+     * @return
+     */
+    int getFrom();
+
+    /**
+     * Returns the greatest character value +1
+     * @return
+     */
+    int getTo();
+
+    /**
+     * Returns true if this and other have common characters
+     * @param other
+     * @return
+     */
+    default boolean intersect(Range other)
+    {
+        return other.accept(getFrom()) || other.accept(getTo()-1) || accept(other.getFrom()) || accept(other.getTo()-1);
+    }
+    
+}
