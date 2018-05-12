4ca94de10b164c2818aebd0a7b70747e81c12282
Timo Vesalainen <timo.vesalainen@iki.fi>
2018-05-12 15:01:31 -0500

    added toString method

--- util/src/main/java/org/vesalainen/math/SimpleAverage.java
+++ util/src/main/java/org/vesalainen/math/SimpleAverage.java
@@ -64,4 +64,10 @@ public class SimpleAverage implements Average
     {
         return sum/count;
     }
+
+    @Override
+    public String toString()
+    {
+        return "SimpleAverage{" + fast() + '}';
+    }
 }
