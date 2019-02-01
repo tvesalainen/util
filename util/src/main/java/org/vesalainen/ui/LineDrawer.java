7d35ae77db9b46ca2f0eb03502311cada78239a4
Timo Vesalainen <timo.vesalainen@iki.fi>
2019-02-01 11:08:16 -0930

    Added LineDrawer class

---
+++ util/src/main/java/org/vesalainen/ui/LineDrawer.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (C) 2019 Timo Vesalainen <timo.vesalainen@iki.fi>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.vesalainen.ui;
+
+import java.awt.Point;
+
+/**
+ * LineDrawer class uses Bresenham's line algorithm to plot line points.
+ * <p>Note! There are no restrictions on lines direction.
+ * @author Timo Vesalainen <timo.vesalainen@iki.fi>
+ */
+public final class LineDrawer
+{
+    /**
+     * Draws line (p1, p2) by plotting points using plot
+     * @param p1
+     * @param p2
+     * @param plot 
+     */
+    public static void drawLine(Point p1, Point p2, PlotOperator plot)
+    {
+        drawLine(p1.x, p1.y, p2.x, p2.y, plot);
+    }
+    /**
+     * Draws line ((x0, y0), (x1, y1)) by plotting points using plot
+     * @param x0
+     * @param y0
+     * @param x1
+     * @param y1
+     * @param plot 
+     */
+    public static void drawLine(int x0, int y0, int x1, int y1, PlotOperator plot)
+    {
+        if (x0 > x1)
+        {
+            drawLine(x1, y1, x0, y0, plot);
+        }
+        else
+        {
+            if (y0 > y1)
+            {
+                drawLine(x0, -y0, x1, -y1, (x, y) -> plot.plot(x, -y));
+            }
+            else
+            {
+                drawLine1(x0, y0, x1, y1, plot);    // ok to go ahead
+            }
+        }
+    }
+
+    private static void drawLine1(int x0, int y0, int x1, int y1, PlotOperator plot)
+    {
+        assert x0 <= x1;
+        assert y0 <= y1;
+        double deltax = x1 - x0;
+        double deltay = y1 - y0;
+        if (deltay <= deltax)
+        {
+            drawLine1(x0, y0, x1, plot, deltay, Math.abs(deltay / deltax));
+        }
+        else
+        {
+            drawLine1(y0, x0, y1, (x, y) -> plot.plot(y, x), deltax, Math.abs(deltax / deltay));
+        }
+    }
+    private static void drawLine1(int x0, int y0, int x1, PlotOperator plot, double deltay, double deltaerr)
+    {
+        double error = 0.0;
+        int y = y0;
+        double signum = Math.signum(deltay);
+        for (int x = x0;x<=x1;x++)
+        {
+            plot.plot(x, y);
+            error += deltaerr;
+            if (error >= 0.5)
+            {
+                y += signum;
+                error -= 1.0;
+            }
+        }
+    }
+}
+
---
+++ util/src/test/java/org/vesalainen/ui/LineDrawerTest.java
@@ -0,0 +1,145 @@
+/*
+ * Copyright (C) 2019 Timo Vesalainen <timo.vesalainen@iki.fi>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.vesalainen.ui;
+
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Timo Vesalainen <timo.vesalainen@iki.fi>
+ */
+public class LineDrawerTest
+{
+    public LineDrawerTest()
+    {
+    }
+
+    @Test
+    public void testHorizontal1()
+    {
+        Point start = new Point(-3, 1);
+        Point end = new Point(3, 1);
+        List<Point> list = new ArrayList<>();
+        LineDrawer.drawLine(start, end, (int x, int y)->list.add(new Point(x,y)));
+        assertEquals(7, list.size());
+        assertEquals(start, list.get(0));
+        assertEquals(end, list.get(6));
+    }
+    @Test
+    public void testHorizontal2()
+    {
+        Point start = new Point(3, 1);
+        Point end = new Point(-3, 1);
+        List<Point> list = new ArrayList<>();
+        LineDrawer.drawLine(start, end, (int x, int y)->list.add(new Point(x,y)));
+        assertEquals(7, list.size());
+        assertEquals(end, list.get(0));
+        assertEquals(start, list.get(6));
+    }
+    @Test
+    public void testVertical1()
+    {
+        Point start = new Point(1, -3);
+        Point end = new Point(1, 3);
+        List<Point> list = new ArrayList<>();
+        LineDrawer.drawLine(start, end, (int x, int y)->list.add(new Point(x,y)));
+        assertEquals(7, list.size());
+        assertEquals(start, list.get(0));
+        assertEquals(end, list.get(6));
+    }
+    @Test
+    public void testVertical2()
+    {
+        Point start = new Point(1, 3);
+        Point end = new Point(1, -3);
+        List<Point> list = new ArrayList<>();
+        LineDrawer.drawLine(start, end, (int x, int y)->list.add(new Point(x,y)));
+        assertEquals(7, list.size());
+        assertEquals(start, list.get(0));
+        assertEquals(end, list.get(6));
+    }
+    @Test
+    public void testDiagonal0()
+    {
+        Point start = new Point(-3, -3);
+        Point end = new Point(3, 3);
+        List<Point> list = new ArrayList<>();
+        LineDrawer.drawLine(start, end, (int x, int y)->list.add(new Point(x,y)));
+        assertEquals(7, list.size());
+        assertEquals(start, list.get(0));
+        assertEquals(end, list.get(6));
+    }
+    @Test
+    public void testDiagonal1()
+    {
+        Point start = new Point(-2, -1);
+        Point end = new Point(6, 3);
+        List<Point> list = new ArrayList<>();
+        LineDrawer.drawLine(start, end, (int x, int y)->list.add(new Point(x,y)));
+        assertEquals(9, list.size());
+        assertEquals(start, list.get(0));
+        assertEquals(end, list.get(8));
+    }
+    @Test
+    public void testDiagonal2()
+    {
+        Point start = new Point(-2, -1);
+        Point end = new Point(3, 6);
+        List<Point> list = new ArrayList<>();
+        LineDrawer.drawLine(start, end, (int x, int y)->list.add(new Point(x,y)));
+        assertEquals(8, list.size());
+        assertEquals(start, list.get(0));
+        assertEquals(end, list.get(7));
+    }
+    @Test
+    public void testDiagonal3()
+    {
+        Point start = new Point(-1, -1);
+        Point end = new Point(6, -3);
+        List<Point> list = new ArrayList<>();
+        LineDrawer.drawLine(start, end, (int x, int y)->list.add(new Point(x,y)));
+        assertEquals(8, list.size());
+        assertEquals(start, list.get(0));
+        assertEquals(end, list.get(7));
+    }
+    @Test
+    public void testDiagonal4()
+    {
+        Point start = new Point(1, 1);
+        Point end = new Point(-3, 6);
+        List<Point> list = new ArrayList<>();
+        LineDrawer.drawLine(start, end, (int x, int y)->list.add(new Point(x,y)));
+        assertEquals(6, list.size());
+        assertEquals(end, list.get(0));
+        assertEquals(start, list.get(5));
+    }
+    @Test
+    public void testDiagonal5()
+    {
+        Point start = new Point(1, 1);
+        Point end = new Point(-3, -6);
+        List<Point> list = new ArrayList<>();
+        LineDrawer.drawLine(start, end, (int x, int y)->list.add(new Point(x,y)));
+        assertEquals(8, list.size());
+        assertEquals(end, list.get(0));
+        assertEquals(start, list.get(7));
+    }
+}
