37d3eb8cd6f848d860cdc6ba9298e34866fe39fb
Timo Vesalainen <timo.vesalainen@iki.fi>
2021-12-10 10:24:22 +1300

    Added LocationCenter

---
+++ util/src/main/java/org/vesalainen/navi/LocationCenter.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2021 Timo Vesalainen <timo.vesalainen@iki.fi>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.vesalainen.navi;
+
+/**
+ *
+ * @author Timo Vesalainen <timo.vesalainen@iki.fi>
+ */
+public class LocationCenter
+{
+    private double latitudeSum = 0;
+    private double sin = 0;
+    private double cos = 0;
+    private long count;
+    
+    public void add(double longitude, double latitude)
+    {
+        latitudeSum += latitude;
+        double rad = Math.toRadians(longitude);
+        sin += Math.sin(rad);
+        cos += Math.cos(rad);
+        count++;
+    }
+    
+    public double latitude()
+    {
+        return latitudeSum/count;
+    }
+    public double longitude()
+    {
+        double atan2 = Math.atan2(sin, cos);
+        return Navis.normalizeToHalfAngle(Math.toDegrees(atan2));
+    }
+
+    @Override
+    public String toString()
+    {
+        return Navis.longitudeString(longitude())+" "+Navis.latitudeString(latitude());
+    }
+    
+}
