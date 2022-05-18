ff6b5f16fe01e8c1033eb981edb48eb7522f0299
tkv <tkv@192.168.214.46>
2022-05-18 14:42:40 +1200

    Removed unnecessary classes

--- can/src/main/java/org/vesalainen/can/dbc/DBC.java
+++ can/src/main/java/org/vesalainen/can/dbc/DBC.java
@@ -30,15 +30,34 @@ public class DBC
 {
     protected static final Map<Integer,MessageClass> canIdMap = new HashMap<>();
     protected static final Map<Integer,MessageClass> pgnMap = new HashMap<>();
+    private static boolean hasJ1939;
+    private static boolean hasN2K;
     
     public static void addJ1939()
     {
         addDBCFile(AbstractCanService.class.getResourceAsStream("/j1939.dbc"));
+        DBC.hasJ1939 = true;
     }
     public static void addN2K()
     {
+        if (!hasJ1939)
+        {
+            addJ1939();
+        }
         addDBCFile(AbstractCanService.class.getResourceAsStream("/n2k.dbc"));
+        DBC.hasN2K = true;
     }
+
+    public static boolean hasJ1939()
+    {
+        return hasJ1939;
+    }
+
+    public static boolean hasN2K()
+    {
+        return hasN2K;
+    }
+    
     public static MessageClass getMessage(int canId)
     {
         return canIdMap.get(canId);
@@ -66,6 +85,7 @@ public class DBC
                     canIdMap.put(mc.getId(), mc);
                 });
                 break;
+            case "J1939":
             case "N2K":
                 dbcFile.forEach((mc)->
                 {
